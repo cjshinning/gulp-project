eb891d6c4b2f1655a4a5ebdca57db1dc368b998d
jie chen <jiechen@jiedeMacBook-Pro.local>
2019-05-22 15:06:40 +0800

    基础构建

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
+var cssnano = require('gulp-cssnano');
+var rename = require('gulp-rename');
+var concat= require('gulp-concat');
+var uglify = require('gulp-uglify');
+var del = require('del');
+var runSequence = require('run-sequence');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+const spritesmith = require('gulp.spritesmith');
 
 // html
 gulp.task('htmlTask', function(){
@@ -9,6 +18,50 @@ gulp.task('htmlTask', function(){
         .pipe(gulp.dest('./dist'))
 })
 
+// 压缩css
+gulp.task('cssTask', function(){
+    gulp.src('./src/css/*.css')
+        .pipe(reload({ stream: true}))
+        .pipe(rename({suffix:'.min'}))
+        .pipe(cssnano())
+        .pipe(gulp.dest('./dist/css'))
+})
+
+// 压缩合并js
+gulp.task('jsTask', function(){
+    gulp.src('./src/js/*.js')
+        .pipe(concat('main.js'))
+        .pipe(rename({suffix:'.min'}))
+        .pipe(reload({ stream: true}))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'))
+})
+
+gulp.task('spritesmith', function() {
+    gulp.src('./src/images/icon/*')
+      .pipe(cache(
+          imagemin()
+      ))  
+      .pipe(spritesmith({
+          imgName: 'sprite.png',
+          cssName: 'sprite.css',
+          padding:5
+      }))
+      .pipe(gulp.dest('./dist/images/icon/'))
+      .pipe(reload({ stream: true}))
+});
+
+// 压缩图片
+gulp.task('imageTask', () =>
+    gulp.src('./src/images/*')
+        .pipe(cache(
+            imagemin()
+        ))
+        .pipe(gulp.dest('./dist/images'))
+        .pipe(reload({ stream: true}))
+);
+
+// 自动刷新
 gulp.task('browserSync', function () {
     browserSync.init({
         server: {
@@ -18,10 +71,18 @@ gulp.task('browserSync', function () {
     });
 });
 
+// 清理dist文件
+gulp.task('clean:dist', function() {
+    return del.sync(['./dist/**/*', '!dist/images', '!dist/images/**/*']);
+});  
+
 gulp.task('watch', function() {
+    // gulp.watch('./src/images/*', ['imageTask']);
+    gulp.watch('./src/css/*.css', ['cssTask']);
+    gulp.watch('./src/js/*.js', ['jsTask']);
     gulp.watch('./src/*.html', ['htmlTask']);
 })
 
-gulp.task('default',['htmlTask','browserSync','watch'],function(){
-    console.log('任务执行结束.')
+gulp.task('default',function(callback){
+    runSequence(['clean:dist','htmlTask','jsTask','cssTask','spritesmith','imageTask','browserSync','watch'],callback)
 })
